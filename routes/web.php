c9abb317163104edb2c42f6c7f9331c1a543f7bc
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-05-26 15:20:14 +0300

    feat(learn): episode 26

--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,12 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+  // \Illuminate\Support\Facades\DB::listen(function($query) {
+  //   logger($query->sql, $query->bindings);
+  // });
   return view('posts', [
-    'posts' => Post::all()
+    // 'posts' => Post::all()
+    'posts' => Post::with('category')->get()
   ]);
 });
 
