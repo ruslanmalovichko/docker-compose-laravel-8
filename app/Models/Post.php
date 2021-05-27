0b7a1971ba3477b291b0d5475ee2dda5af9bf9e1
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-05-27 10:39:21 +0300

    feat(learn): episode 27

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -21,5 +21,10 @@ class Post extends Model
   {
     return $this->belongsTo(Category::class);
   }
+
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,43 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+      User::truncate();
+      Category::truncate();
+      Post::truncate();
+
+      $user = User::factory()->create();
+
+      $personal = Category::create([
+        'name' => 'Personal',
+        'slug' => 'personal'
+      ]);
+
+      $family = Category::create([
+        'name' => 'Family',
+        'slug' => 'family'
+      ]);
+
+      $work = Category::create([
+        'name' => 'Work',
+        'slug' => 'work'
+      ]);
+
+      Post::create([
+        'user_id' => $user->id,
+        'category_id' => $family->id,
+        'title' => 'My Family Post',
+        'slug' => 'my-first-post',
+        'excerpt' => '<p>Lorem ipsum excerpt 1</p>',
+        'body' => '<p>Lorem ipsum body 1</p>'
+      ]);
+
+      Post::create([
+        'user_id' => $user->id,
+        'category_id' => $work->id,
+        'title' => 'My Work Post',
+        'slug' => 'my-work-post',
+        'excerpt' => '<p>Lorem ipsum excerpt 2</p>',
+        'body' => '<p>Lorem ipsum body 2</p>'
+      ]);
     }
 }
