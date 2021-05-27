b81e521fecb41b14f200a243b19fd0e2859555ca
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-05-27 17:29:53 +0300

    fix: fix code formatting

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,17 @@
 <x-layout>
-        @include('_posts-header')
-
-
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card />
-
-            <div class="lg:grid lg:grid-cols-2">
-              <x-post-card />
-              <x-post-card />
-            </div>
-
-            <div class="lg:grid lg:grid-cols-3">
-              <x-post-card />
-              <x-post-card />
-              <x-post-card />
-            </div>
-        </main>
-
+  @include('_posts-header')
+  <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+    <x-post-featured-card />
+    <div class="lg:grid lg:grid-cols-2">
+      <x-post-card />
+      <x-post-card />
+    </div>
+    <div class="lg:grid lg:grid-cols-3">
+      <x-post-card />
+      <x-post-card />
+      <x-post-card />
+    </div>
+  </main>
 
   @foreach ($posts as $post)
     <article class="{{ $loop->even ? 'foobar' : '' }}">
