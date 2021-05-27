e217de32c76b00251df01f5d85b0996c34ba9004
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-05-27 13:47:24 +0300

    feat(learn): episode 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,43 +16,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-      User::truncate();
-      Category::truncate();
-      Post::truncate();
+      // User::truncate();
+      // Category::truncate();
+      // Post::truncate();
 
-      $user = User::factory()->create();
-
-      $personal = Category::create([
-        'name' => 'Personal',
-        'slug' => 'personal'
+      $user = User::factory()->create([
+        'name' => 'John Doe'
       ]);
 
-      $family = Category::create([
-        'name' => 'Family',
-        'slug' => 'family'
+      Post::factory(5)->create([
+        'user_id' => $user->id
       ]);
 
-      $work = Category::create([
-        'name' => 'Work',
-        'slug' => 'work'
-      ]);
+      // Post::factory(5)->create();
 
-      Post::create([
-        'user_id' => $user->id,
-        'category_id' => $family->id,
-        'title' => 'My Family Post',
-        'slug' => 'my-first-post',
-        'excerpt' => '<p>Lorem ipsum excerpt 1</p>',
-        'body' => '<p>Lorem ipsum body 1</p>'
-      ]);
+      // $user = User::factory()->create();
 
-      Post::create([
-        'user_id' => $user->id,
-        'category_id' => $work->id,
-        'title' => 'My Work Post',
-        'slug' => 'my-work-post',
-        'excerpt' => '<p>Lorem ipsum excerpt 2</p>',
-        'body' => '<p>Lorem ipsum body 2</p>'
-      ]);
+      // $personal = Category::create([
+      //   'name' => 'Personal',
+      //   'slug' => 'personal'
+      // ]);
+
+      // $family = Category::create([
+      //   'name' => 'Family',
+      //   'slug' => 'family'
+      // ]);
+
+      // $work = Category::create([
+      //   'name' => 'Work',
+      //   'slug' => 'work'
+      // ]);
+
+      // Post::create([
+      //   'user_id' => $user->id,
+      //   'category_id' => $family->id,
+      //   'title' => 'My Family Post',
+      //   'slug' => 'my-first-post',
+      //   'excerpt' => '<p>Lorem ipsum excerpt 1</p>',
+      //   'body' => '<p>Lorem ipsum body 1</p>'
+      // ]);
+
+      // Post::create([
+      //   'user_id' => $user->id,
+      //   'category_id' => $work->id,
+      //   'title' => 'My Work Post',
+      //   'slug' => 'my-work-post',
+      //   'excerpt' => '<p>Lorem ipsum excerpt 2</p>',
+      //   'body' => '<p>Lorem ipsum body 2</p>'
+      // ]);
     }
 }
