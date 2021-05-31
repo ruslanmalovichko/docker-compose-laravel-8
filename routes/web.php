24038dae2f3464b128cd0789abb49c9dc4aa16f3
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-05-31 17:41:19 +0300

    feat(learn): episode 35

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/', function () {
     'posts' => Post::latest()->get(),
     'categories' => Category::all()
   ]);
-});
+})->name('home');
 
 // Route::get('posts/{post}', function (Post $post) {
 Route::get('posts/{post:slug}', function (Post $post) {
@@ -45,7 +45,7 @@ Route::get('categories/{category:slug}', function (Category $category) {
     'currentCategory' => $category,
     'categories' => Category::all()
   ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
   return view('posts', [
