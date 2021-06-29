c9b019ec2f28c9c071e97e2c65e8b07454fae5d3
Ruslan Malovichko <ruslan.malovichko@wearebrain.com>
2021-06-29 18:06:57 +0300

    feat(learn): episode 45

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+  public function create()
+  {
+    return view('register.create');
+  }
+
+  public function store()
+  {
+    // var_dump(request()->all());
+    // return request()->all();
+    // create the user
+    $attributes = request()->validate([
+      'name' => 'required|max:255',
+      'username' => 'required|max:255|min:3',
+      'email' => 'required|email|max:255',
+      'password' => 'required|min:7|max:255'
+    ]);
+
+    User::create($attributes);
+
+    return redirect('/');
+  }
+}
+
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
 use App\Http\Controllers\PostController;
-use App\Models\Post;
-use App\Models\Category;
-use App\Models\User;
-use Illuminate\Support\Facades\File;
+use App\Http\Controllers\RegisterController;
+// use App\Models\Post;
+// use App\Models\Category;
+// use App\Models\User;
+// use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+// use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,6 +22,8 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
 
 // Route::get('categories/{category:slug}', function (Category $category) {
 //   return view('posts', [
